a8449f8e82631696df06309c4c29d884c0c8afae
Amit Tiparadi <amit.tiparadi1@gmail.com>
2026-06-18 22:19:42 +0530

    Code commited for the StaticDropDown page

--- tests/StaticDropDown.spec.js
+++ tests/StaticDropDown.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require ('@playwright/test');
 
-test.only('Check DropDown Test', async ({page})=>{
+test('Check DropDown Test', async ({page})=>{
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = page.locator("#username");
@@ -17,9 +17,32 @@ test.only('Check DropDown Test', async ({page})=>{
     await page.locator("#terms").click();
     await expect(page.locator("#terms")).toBeChecked();
     await page.locator("#terms").uncheck();
+    //Here in below line action is performed insided hence await is added inside
     expect(await page.locator("#terms").isChecked()).toBeFalsy;
     (await expect(pageLink).toHaveAttribute("class", 'blinkingText'));//Will check the link present on page
     //await page.pause();
+});
 
+test.only('Child window handling', async ({browser})=>{
+    const context =  await browser.newContext();
+    const page = await context.newPage();
+    const userName = page.locator("#username");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const pageLink= page.locator("[href*= 'documents-request']");
 
-})
+    const [newPage]= await Promise.all(
+    [
+    context.waitForEvent('page'),//listener for any new page to open, this will be added just before the action on the new page
+    await pageLink.click(),//new page is opened
+
+    ])
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0]
+    console.log(text);
+    //console.log(domain);
+    await page.locator("#username").fill(domain);
+    await page.pause();
+    console.log(await page.locator("#username").inputValue());
+    
+});
